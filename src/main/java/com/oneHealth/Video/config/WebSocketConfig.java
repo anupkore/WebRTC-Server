abdebdc7b081a785d92ec858cb71ef0f5f5a72fc
unknown <anup.kore88@gmail.com>
2023-08-11 09:24:41 +0530

    WebSocket Changes

--- src/main/java/com/oneHealth/Video/config/WebSocketConfig.java
+++ src/main/java/com/oneHealth/Video/config/WebSocketConfig.java
@@ -16,7 +16,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/websocket").setAllowedOriginPatterns("*").withSockJS();
+		//registry.addEndpoint("/websocket").setAllowedOriginPatterns("*").withSockJS();
+		registry.addEndpoint("/websocket").setAllowedOriginPatterns("*");
 	}
 
 	@Override
